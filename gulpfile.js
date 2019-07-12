94a845ddc2beb5a48f03379bc9bd9fcd27620167
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-12 10:29:29 +0200

    using gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ gulp.task("processJS",()=>{
         .pipe(gulp.dest("dist"));
 });
 
+// using JShint for linting my code
 const jshint = require("gulp-jshint");
 
 gulp.task("processJS", () => {
@@ -23,3 +24,20 @@ gulp.task("processJS", () => {
         .pipe(jshint.reporter("default"))
         .pipe(gulp.dest("dist"));
 });
+
+
+// using gulp-babel for transpiling my code
+const babel = require("gulp-babel");
+
+gulp.task("processJS", () => {
+    gulp.src("*.js")
+        .pipe(jshint({
+            esversion: 6
+        }))
+        .pipe(jshint.reporter("default"))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest("dist"));
+});
+
