139d9fb6f323ec23a10b9db90ef07a87249a4a12
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-12 10:12:06 +0200

    use jshint for linting

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,14 @@ gulp.task("processJS",()=>{
     gulp.src("*.js")
         .pipe(gulp.dest("dist"));
 });
+
+const jshint = require("gulp-jshint");
+
+gulp.task("processJS", () => {
+    gulp.src("*.js")
+        .pipe(jshint({
+            esversion: 6
+        }))
+        .pipe(jshint.reporter("default"))
+        .pipe(gulp.dest("dist"));
+});
