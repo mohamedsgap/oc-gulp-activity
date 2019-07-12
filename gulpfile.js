a97c28baa6a6e4c2b16951703070dfa29abebb36
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-07-12 10:44:37 +0200

    babel polyfill issue

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task("processJS", () => {
 const babel = require("gulp-babel");
 
 gulp.task("processJS", () => {
-    gulp.src("*.js")
+    gulp.src("blog.js")
         .pipe(jshint({
             esversion: 6
         }))
@@ -41,3 +41,22 @@ gulp.task("processJS", () => {
         .pipe(gulp.dest("dist"));
 });
 
+gulp.task("processJS", () => {
+    gulp.src("functions.js")
+        .pipe(jshint({
+            esversion: 6
+        }))
+        .pipe(jshint.reporter("default"))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest("dist"));
+});
+
+//// babel-polyfill issue. 
+//// set up another task to copy  browser.js  to our  dist  folder.
+
+gulp.task("babelPolyfill", () => {
+    gulp.src('node_modules/babel-polyfill/browser.js')
+        .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
+});
